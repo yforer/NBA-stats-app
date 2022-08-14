487a1757b55c623b78203c105b79ebfcba9b3df2
yuvalforer <yuvalforer@gmail.com>
2022-08-14 21:11:43 +0300

    merging conflicts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import "./index.scss";
+import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
